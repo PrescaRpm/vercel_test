fefb25662945e3695fec3c160d721da8a563162a
PrescaRpm <165167993+PrescaRpm@users.noreply.github.com>
2026-03-31 19:40:47 +0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from modules.bio_provider import get_biography_data
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Mengambil data dari data layer (Clean Architecture)
+    my_bio = get_biography_data()
+    return render_template('index.html', data=my_bio)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ modules/bio_provider.py
@@ -0,0 +1,10 @@
+def get_biography_data():
+    return {
+        "name": "Rifky Putra Mahardika",
+        "nickname": "Dika",
+        "major": "Teknologi Informasi",
+        "university": "Universitas Lambung Mangkurat",
+        "origin": "Tanjung, Tabalong",
+        "interest": "UI/UX & Big Data",
+        "motto": "Membangun solusi digital dari Kalimantan untuk dunia."
+    }
