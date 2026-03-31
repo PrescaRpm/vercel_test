5ea058e334a613ea6ec9c93ce4f9e3c1631f9336
PrescaRpm <165167993+PrescaRpm@users.noreply.github.com>
2026-03-31 20:06:09 +0800

    Update bio_provider.py

--- modules/bio_provider.py
+++ modules/bio_provider.py
@@ -3,7 +3,7 @@ def get_biography_data():
         "name": "Rifky Putra Mahardika",
         "nickname": "Mahardika",
         "major": "Teknologi Informasi",
-        "university": "Gadjah Mada",
+        "university": "Harvard Cihuyyyy",
         "origin": "Tanjung, Tabalong",
         "interest": "UI/UX & Big Data",
         "motto": "Membangun solusi digital dari Kalimantan untuk dunia."
